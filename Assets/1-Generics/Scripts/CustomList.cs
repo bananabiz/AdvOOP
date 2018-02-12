fd0bbc9ef5ef294df6111f8a0b612fd7b4bd1c1f
bananabiz <lee21acc973@163.com>
2018-02-12 16:03:58 +1100

    Generics - add 2 scripts

---
+++ Assets/1-Generics/Scripts/CustomList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    public class CustomList<T>
+    {
+        public T[] data;
+        public int amount;
+
+        public CustomList() { amount = 0; }
+
+        public void Add(T item)
+        {
+            //Create a new array of amount + 1
+            T[] cache = new T[amount + 1];
+            //Check if the list has been initialized
+            if (data != null)
+            {
+                //Copy all existing items to new array
+                for (int i = 0; i < data.Length; i++)
+                {
+                    //Copy each element to cache
+                    cache[i] = data[i];
+                }
+            }
+            //Set the end element to new item
+            cache[amount] = item;
+            //Point to new array
+            data = cache;
+            //Increment amount
+            amount++;
+        }
+        
+    }
+}
